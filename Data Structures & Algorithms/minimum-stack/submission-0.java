17a5169ea6016b079a37af4d2cc4e9c5a487ed8c
Paul Ged <pauliusgedrimas@gmail.com>
2026-05-28 15:47:23 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,40 @@
+class MinStack {
+    ArrayList<Integer> stack;
+
+    public MinStack() {
+        this.stack = new ArrayList<Integer>();
+    }
+    
+    public void push(int val) {
+        this.stack.add(val);
+    }
+    
+    public void pop() {
+        if (this.stack.size() != 0){
+            this.stack.remove(this.stack.size() - 1);
+        }
+    }
+    
+    public int top() {
+        if (this.stack.size() != 0){
+            return this.stack.get(this.stack.size() - 1);
+        }
+        return -1;
+    }
+    
+    public int getMin() {
+        if (this.stack.size() != 0){
+            int min = this.stack.get(0);
+            for (int i = 1; i < this.stack.size(); i++){
+                if (this.stack.get(i) < min){
+                    min = this.stack.get(i);
+                }
+            }
+            return min;
+        }
+        else {
+            return -1;
+        }
+
+    }
+}
