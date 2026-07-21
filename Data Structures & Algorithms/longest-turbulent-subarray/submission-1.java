f7ce3806530c8275b045b64499039b86bec7c3a7
Paul Ged <pauliusgedrimas@gmail.com>
2026-07-21 16:40:59 -0700

    Add: longest-turbulent-subarray - submission-1

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        int l = 0;
+        int r = 0;
+        int max = 0;
+        while (r<arr.length){
+            if (r - 2 >= 0 && r - 2 >= l){
+                if (arr[r - 2] > arr[r - 1] && arr[r-1] > arr[r]){
+                    l = r - 1;
+                }
+                if (arr[r-2] < arr[r - 1] && arr[r-1] < arr[r]){
+                    l = r - 1;
+                }
+            }
+            if (r != l && arr[r] == arr[r-1]){
+                l = r;
+            }
+            max = Math.max(max, r-l+1);
+            r++;
+        }
+        return max;
+    }
+}
